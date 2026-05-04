e74376ea371b82770d187a06616916865c6910dc
Elio Scarton <elio@mcands.com.au>
2026-05-04 13:25:19 +0000

    Renumber GlobalAccountMappingHint migration to 0128 to resolve conflict with 0127_expand_knowledge_category_choices

--- core/migrations/0128_globalaccountmappinghint.py
+++ core/migrations/0128_globalaccountmappinghint.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0126_userstyleprofile_evaagenttrace_evacommentaryedit_and_more'),
+        ('core', '0127_expand_knowledge_category_choices'),
     ]
 
     operations = [
